2a541bc02379dd0fc54bdd6fd51ae20b44be21d3
Richard Fages <richard.fages@gmail.com>
2018-09-12 14:50:50 +0200

    Ajout page

---
+++ app/src/main/java/fr/wildcodeschool/monsterwiki/profilMonster.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.monsterwiki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class profilMonster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_monster);
+    }
+}
