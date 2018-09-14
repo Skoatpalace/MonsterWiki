9129364b983c089e397bdd846f6898104cbd7f63
Pierre Fajardo <pierrefaja@gmail.com>
2018-09-14 13:54:57 +0200

    Page stats

---
+++ app/src/main/java/fr/wildcodeschool/monsterwiki/Stats.java
@@ -0,0 +1,25 @@
+package fr.wildcodeschool.monsterwiki;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
+
+public class Stats extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slide_layout);
+
+
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/fr/wildcodeschool/monsterwiki/profilMonster.java
+++ app/src/main/java/fr/wildcodeschool/monsterwiki/profilMonster.java
@@ -1,13 +1,19 @@
 package fr.wildcodeschool.monsterwiki;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class profilMonster extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_monster);
+
+        ViewPager slideDesc = findViewById(R.id.slideMonster);
+        slideDesc.setAdapter(new StatsPageAdapter(profilMonster.this));
     }
 }
+
